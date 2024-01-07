7a2b8824d402e61f47261746aecfb01490a2ab83
Dan Lister <lister.daniel@gmail.com>
2024-01-07 11:44:08 +1000

    Day Five Part A Complete

--- day_five.c
+++ day_five.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "string_utilities.h"
 
+typedef long long int big_int;
+
+#define BIG_INT_FORMAT "%lld"
+#define BIG_INT_PARSE(x) atoll(x)
+
 const int HEADINGS = 7;
 
 int count_seeds(char *content) {
@@ -18,11 +23,11 @@ int count_seeds(char *content) {
 	return total;
 }
 
-void collect_seeds(char *content, int amount, int seeds[amount]) {
+void collect_seeds(char *content, int amount, big_int seeds[amount]) {
 	while (*content != '\n') {
 		content = skip_to_number(content);
 		if (*content != '\n') {
-			*seeds = atoi(content);
+			*seeds = BIG_INT_PARSE(content);
 			seeds++;
 		} else {
 			return;
@@ -32,7 +37,7 @@ void collect_seeds(char *content, int amount, int seeds[amount]) {
 	}
 }
 
-void populate_map(char *content, int heading_index[HEADINGS], int rows, int mappings[rows][3]) {
+void populate_map(char *content, int heading_index[HEADINGS], int rows, big_int mappings[rows][3]) {
 	int line_count = 0;
 	while (*content != '\0') {
 		// eat gap
@@ -46,13 +51,13 @@ void populate_map(char *content, int heading_index[HEADINGS], int rows, int mapp
 		// start collecting numbers until terminus or blank line
 		while (*content != '\n' && *content != '\0') {
 			// collect three numbers
-			mappings[line_count][0] = atoi(content);
+			mappings[line_count][0] = BIG_INT_PARSE(content);
 			content = skip_to_tokens(content, " ");
 			content = skip_to_number(content);
-			mappings[line_count][1] = atoi(content);
+			mappings[line_count][1] = BIG_INT_PARSE(content);
 			content = skip_to_tokens(content, " ");
 			content = skip_to_number(content);
-			mappings[line_count][2] = atoi(content);
+			mappings[line_count][2] = BIG_INT_PARSE(content);
 			line_count++;
 			// skip to end of line
 			content = skip_to_tokens(content, "\n\0");
@@ -62,40 +67,37 @@ void populate_map(char *content, int heading_index[HEADINGS], int rows, int mapp
 	}
 }
 
-int map_seed(int seed, int current_heading, int heading_index[HEADINGS], int lines, int mapping[lines][3]) {
-	if (current_heading == HEADINGS)
+big_int map_seed(big_int seed, int current_heading, int heading_index[HEADINGS], int lines, big_int mapping[lines][3]) {
+	if (current_heading == HEADINGS) {
 		return seed;
+	}
 	int section_end = current_heading == HEADINGS - 1 ? lines : heading_index[current_heading + 1];
 	// see if seed maps to range
 	for (int i = heading_index[current_heading];i < section_end;i++) {
-		printf("Checking seed %d fits between %d and %d: ", seed, mapping[i][1], mapping[i][1] + mapping[i][2]);
 		if (seed >= mapping[i][1] && seed < mapping[i][1] + mapping[i][2]) {
-			int offset = seed - mapping[i][1];
-			printf("destination: \n");
+			big_int offset = seed - mapping[i][1];
 			return map_seed(mapping[i][0] + offset, current_heading + 1, heading_index, lines, mapping);
-		} else {
-			printf("no\n");
 		}
 	}
 	// didn't find it, direct mapping
 	return map_seed(seed, current_heading + 1, heading_index, lines, mapping);
 }
 
-int part_a(char *contents) {
+big_int part_a(char *contents) {
 	// seeds
 	int seed_count = count_seeds(contents);
-	int seeds[seed_count];
+	big_int seeds[seed_count];
 	collect_seeds(contents, seed_count, seeds);
 	printf("Seeds: ");
 	for (int i = 0;i < seed_count;i++) {
-		printf("%d%c", seeds[i], i == seed_count - 1 ? '\n' : ',');
+		printf(BIG_INT_FORMAT "%c", seeds[i], i == seed_count - 1 ? '\n' : ',');
 	}
 	// mapping
 	contents = skip_to_tokens(contents, "\n");
 	contents++;
 	int lines = count_string_token(contents, '\n') - 14;
 	printf("Total Lines: %d\n", lines);
-	int mappings[lines][3];
+	big_int mappings[lines][3];
 	int heading_index[HEADINGS];
 	populate_map(contents, heading_index, lines, mappings);
 	int hi = 0;
@@ -104,14 +106,17 @@ int part_a(char *contents) {
 			printf("\n");
 			hi++;
 		}
-		printf("%d %d %d\n", mappings[i][0], mappings[i][1], mappings[i][2]);
+		printf(BIG_INT_FORMAT " " BIG_INT_FORMAT " " BIG_INT_FORMAT "\n", mappings[i][0], mappings[i][1], mappings[i][2]);
 	}
 	// find smallest out of seed mappings
-	int min = -1;
-	for (int i = 0;i < 1;i++) {
-		int res = map_seed(seeds[i], 0, heading_index, lines, mappings);
-		min = min == -1 || res < min ? res : min;
+	big_int min = map_seed(seeds[0], 0, heading_index, lines, mappings);
+	for (int i = 1;i < seed_count;i++) {
+		printf("Seed " BIG_INT_FORMAT " -> ", seeds[i]);
+		big_int res = map_seed(seeds[i], 0, heading_index, lines, mappings);
+		printf(BIG_INT_FORMAT "\n", res);
+		min = res < min ? res : min;
 	}
+	printf("Min: " BIG_INT_FORMAT "\n", min);
 	return min;
 }
 
@@ -129,7 +134,7 @@ int main(int argc, char **argv) {
 	if (argc > 1 && argv[1][0] == 'b') {
 		printf("Part B Total: %d\n", part_b(contents));
 	} else {
-		printf("Part A Total: %d\n", part_a(contents));
+		printf("Part A Total: " BIG_INT_FORMAT "\n", part_a(contents));
 	}
 	free(contents);
 }
